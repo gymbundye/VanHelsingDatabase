148f16207a1ad76edac1d06043590fdc901f30eb
gymbundye <84460968+gymbundye@users.noreply.github.com>
2023-11-02 12:07:27 -0400

    worked on services

--- VanHelsingDatabase/Client/Services/MonsterService/MonsterService.cs
+++ VanHelsingDatabase/Client/Services/MonsterService/MonsterService.cs
@@ -1,18 +1,28 @@
-﻿namespace VanHelsingDatabase.Client.Services.MonsterService
+﻿using System.Net.Http.Json;
+
+namespace VanHelsingDatabase.Client.Services.MonsterService
 {
     public class MonsterService : IMonsterService
     {
-        public List<Monster> Monsters { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public List<Location> Locations { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private readonly HttpClient _http;
+        public MonsterService(HttpClient http) 
+        {
+            _http = http;
+        }
+        public List<Monster> Monsters { get; set; } = new List<Monster>();
+        public List<Location> Locations { get; set; } = new List<Location>();
 
         public Task GetLocations()
         {
             throw new NotImplementedException();
         }
 
-        public Task GetMonsters()
+        public async Task GetMonsters()
         {
-            throw new NotImplementedException();
+            var result = await _http.GetFromJsonAsync<List<Monster>>("api/monster");
+            if (result != null)
+                Monsters = result; 
+
         }
 
         public Task<Monster> GetOneMonster(int id)
